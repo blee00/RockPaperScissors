8e0e5926f2461dc26a1f7eda36dc28e016bcacfa
Benson Lee <BensonL1334@gmail.com>
2022-12-31 22:58:08 -0500

    Receive player choice from input and display result

--- RPS.js
+++ RPS.js
@@ -4,38 +4,54 @@ function getComputerChoice() {
     return roshambo[Math.floor(Math.random()*3)];
 };
 
+let displayResult = document.querySelector('.result');
+
 function playRound(playSelection, computerSelection) {
-    playSelection = prompt('Type your choice of RPS against the computer', '');
-    computerSelection = getComputerChoice();
-    if (playSelection.toLowerCase() === roshambo[0]) {
-        if (computerSelection === roshambo[1]) {
-            console.log(`You lose! ${roshambo[1]} beats ${roshambo[0]}.`);
-        } else if (computerSelection === roshambo[2]) {
-            console.log(`You win! ${roshambo[0]} beats ${roshambo[2]}.`);
-        } else {
-            console.log("It's a tie!");
-        }
-    } else if (playSelection.toLowerCase() === roshambo[1]) {
-        if (computerSelection === roshambo[2]) {
-            console.log(`You lose! ${roshambo[2]} beats ${roshambo[1]}.`);
-        } else if (computerSelection === roshambo[0]) {
-            console.log(`You win! ${roshambo[1]} beats ${roshambo[0]}.`);
-        } else {
-            console.log("It's a tie!");
-        }
-    } else if (playSelection.toLowerCase() === roshambo[2]){
-        if (computerSelection === roshambo[0]) {
-            console.log(`You lose! ${roshambo[0]} beats ${roshambo[2]}.`);
-        } else if (computerSelection === roshambo[1]) {
-            console.log(`You win! ${roshambo[2]} beats ${roshambo[1]}.`);
-        } else {
-            console.log("It's a tie!");
-        }
+    let myButton = document.getElementById("myBtn");
+    let userInput = document.getElementById("userChoice");
+    myButton.addEventListener("click", () => {
+         computerSelection = getComputerChoice();
+         playSelection = userInput.value;
+         if (playSelection.toLowerCase() === roshambo[0]) {
+             if (computerSelection === roshambo[1]) {
+                displayResult.innerText = `You lose! ${roshambo[1]} beats ${roshambo[0]}.`;
+             } else if (computerSelection === roshambo[2]) {
+                displayResult.innerText = `You win! ${roshambo[0]} beats ${roshambo[2]}.`;
+             } else {
+                displayResult.innerText = "It's a tie!";
+             }
+         } else if (playSelection.toLowerCase() === roshambo[1]) {
+             if (computerSelection === roshambo[2]) {
+                displayResult.innerText = `You lose! ${roshambo[2]} beats ${roshambo[1]}.`;
+             } else if (computerSelection === roshambo[0]) {
+                displayResult.innerText = `You win! ${roshambo[1]} beats ${roshambo[0]}.`;
+             } else {
+                displayResult.innerText = "It's a tie!";
+             }
+         } else if (playSelection.toLowerCase() === roshambo[2]){
+             if (computerSelection === roshambo[0]) {
+                displayResult.innerText = `You lose! ${roshambo[0]} beats ${roshambo[2]}.`
+             } else if (computerSelection === roshambo[1]) {
+                displayResult.innerText = `You win! ${roshambo[2]} beats ${roshambo[1]}.`
+             } else {
+                displayResult.innerText = "It's a tie!"
+             }
+        }; 
+      });
     };
-};
+
+
+
+
+//   getComputerChoice();
+//   choice();
+//   console.log(getComputerChoice());
 
 getComputerChoice();
 playRound();
+
+  
+
 
 
 
